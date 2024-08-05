6465dcbf391b718f68cb82669049e6f073c5b55d
Namanjain2213 <nj626433@gmail.com>
2024-08-05 15:31:32 +0530

    changes in list navbar and card

--- Frontend/src/Components/Card.jsx
+++ Frontend/src/Components/Card.jsx
@@ -6,10 +6,10 @@ function Card(props) {
         <>
             <div className='' >
                 <div class=" flex ml-2  gap-4 shadow-2xl flex-col  dark:bg-slate-900 dark:text-white rounded-xl bg-white mt-4  text-gray-700 ">
-                    <div class=" mx-4 mt-4 h-96 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700">
+                    <div class=" mx-4 mt-4 h-full overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700">
                         <img
                             src={data.image}
-                            class="h-full w-full object-fill"
+                            class="h-full w-full object-cover bg-transparent "
                         />
                     </div>
                     <div class="p-6">
@@ -28,6 +28,7 @@ function Card(props) {
                            category: <span className='bg-error px-4 py-1 rounded-full text-white font-bold ' > {data.category} </span> 
                         </p>
                     </div>
+                    <hr />
                     <div class="p-6 pt-0">
                         <button
                             class="block w-full select-none rounded-lg bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
