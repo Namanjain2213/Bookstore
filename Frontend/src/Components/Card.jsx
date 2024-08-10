bc31b55704c76d311d1aa46b6142098885aa719f
Namanjain2213 <nj626433@gmail.com>
2024-08-10 22:27:49 +0530

    new commit

--- Frontend/src/Components/Card.jsx
+++ Frontend/src/Components/Card.jsx
@@ -9,22 +9,22 @@ function Card(props) {
                     <div class=" mx-4 mt-4 h-full overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700">
                         <img
                             src={data.image}
-                            class="h-full w-full object-cover bg-transparent "
+                            className="h-[250px] w-full object-cover bg-transparent "
                         />
                     </div>
-                    <div class="p-6">
-                        <div class="mb-2 flex items-center justify-between">
-                            <p class="block font-sans text-xl font-medium leading-relaxed dark:text-sky-400 text-black ">
+                    <div className="p-6">
+                        <div className="mb-2 flex items-center justify-between">
+                            <p className="block font-sans text-xl font-medium leading-relaxed dark:text-sky-400 text-black ">
                                 {data.name}
                             </p>
-                            <p class="block font-sans text-base font-medium leading-relaxed dark:text-sky-400 text-black ">
+                            <p className="block font-sans text-base font-medium leading-relaxed dark:text-sky-400 text-black ">
                             ₹ {data.price}   
                             </p>
                         </div>
-                        <p class="block font-sans text-xl font-bold leading-normal dark:text-white  text-black  ">
+                        <p className="block font-sans text-xl font-bold leading-normal dark:text-white  text-black  ">
                             {data.title}
                         </p>
-                        <p class=" font-sans w-full  text-base md:text-lg md:font-bold leading-normal mt-3 dark:text-white text-black  ">
+                        <p className=" font-sans w-full  text-base md:text-lg md:font-bold leading-normal mt-3 dark:text-white text-black  ">
                            Category: <span className='bg-error px-4 py-1 rounded-full text-white font-bold ' > {data.category} </span> 
                         </p>
                     </div>
@@ -32,7 +32,7 @@ function Card(props) {
                     <div class=" ">
                     <hr />
                         <button
-                            class="block w-full mt-4 mb-4 select-none rounded-lg bg-blue-gray-900/10 py-1 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                            className="block w-full mt-4 mb-4 select-none rounded-lg bg-blue-gray-900/10 py-1 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                             type="button"
                         >
                             Buy now
